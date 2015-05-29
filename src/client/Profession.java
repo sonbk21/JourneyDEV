10fd537d2baa992a980e9ad419d4e8632c0e353e
SYJourney <ysfoig2153tg@hotmail.de>
2015-05-29 20:12:49 +0200

    update

--- src/client/Profession.java
+++ src/client/Profession.java
@@ -6,28 +6,32 @@
 
 package client;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import tools.DatabaseConnection;
 import tools.Pair;
 
 /**
  * JourneyMS
  * 
  */
-public class Profession {
+public class Professions {
     
     private Pair<MapleProfession, MapleProfession> professions;
-    private Pair<Integer, Integer> exp;
-    private Pair<ProfessionLevel, ProfessionLevel> level;
     
-    public Profession() {
-        professions.left = MapleProfession.NONE;
-        professions.right = MapleProfession.NONE;
-        exp.left = 0;
-        exp.right = 0;
-        level.left = ProfessionLevel.NEWBIE;
-        level.right = ProfessionLevel.NEWBIE;
+    public Professions(MapleProfession left, MapleProfession right) {
+        professions.left = left;
+        professions.right = right;
     }
     
-    public enum MapleProfession {
+    public Professions() {
+        professions.left = null;
+        professions.right = null;
+    }
+    
+    public enum ProfessionType {
         NONE, MINING, HERBALISM, CRAFTING, ALCHEMY;
         
         public String getName() {
@@ -62,20 +66,118 @@ public class Profession {
         }
     }
     
-    public void gainExp(MapleProfession type, int gain, int chrlevel) {
-        boolean left = professions.getLeft() == type;
-        if (left) {
-            exp.left += gain;
-            if (exp.left >= level.left.getNext().getThreshold() && chrlevel >= level.left.getNext().getLevelReq())
-                level.left = level.left.getNext();
-        } else {
-            exp.right += gain;
-            if (exp.right >= level.right.getNext().getThreshold() && chrlevel >= level.left.getNext().getLevelReq())
-                level.right = level.right.getNext();
+    public class MapleProfession {
+        
+        private final ProfessionType type;
+        private ProfessionLevel level;
+        private Integer exp;
+        
+        public MapleProfession(ProfessionType type, ProfessionLevel level, int exp) {
+            this.type = type;
+            this.level = level;
+            this.exp = exp;
+        }
+        
+        public boolean gainExp(int gain, short chrlevel) {
+            exp += gain;
+            if (exp >= level.getNext().getThreshold() && chrlevel >= level.getNext().getLevelReq()) {
+                level = level.getNext();
+                return true;
+            }
+            return false;
+        }
+        
+        public String getName(boolean left) {
+            return type.getName();
+        }
+        
+        public ProfessionType getType() {
+            return type;
+        }
+        
+        public ProfessionLevel getLevel() {
+            return level;
+        }
+        
+        public int getExp() {
+            return exp;
         }
     }
     
-    public String getName(boolean left) {
-        return (left)?professions.getLeft().getName():professions.getRight().getName();
+    public void changeProfession(ProfessionType oldtype, ProfessionType newtype) {
+        if (professions.left.getType() == oldtype)
+            professions.left = new MapleProfession(newtype, ProfessionLevel.NEWBIE, 0);
+        else
+            professions.right = new MapleProfession(newtype, ProfessionLevel.NEWBIE, 0);
+    }
+    
+    public MapleProfession getProfession(ProfessionType type) {
+        if (professions.left == null)
+            return null;
+        
+        if (professions.left.getType() == type)
+            return professions.left;
+        else
+            return professions.right;
+    }
+    
+    public void loadProfessions(final int charid) {
+        try (PreparedStatement ps = DatabaseConnection.getConnection().prepareStatement("SELECT type, level, exp FROM professions WHERE charid = ?")) {
+            ps.setInt(1, charid);
+            try (ResultSet rs = ps.executeQuery()) {
+                boolean first = true;
+                while (rs.next()) {
+                    MapleProfession loaded = new MapleProfession(ProfessionType.values()[rs.getByte("type")], ProfessionLevel.values()[rs.getByte("level")], rs.getInt("exp"));
+                    if (first) {
+                        professions.left = loaded;
+                        first = false;
+                    } else {
+                        professions.right = loaded;
+                    }
+                }
+            }
+        } catch (SQLException sqle) {
+            System.out.println("Error."+sqle);
+        }
+    }
+
+    public void saveProfessions(final int charid) {
+        if (professions.left == null) {
+            return;
+        }
+        
+        try {
+            Connection con = DatabaseConnection.getConnection();
+            PreparedStatement ps = con.prepareStatement("DELETE FROM professions WHERE charid = ?");
+            ps.setInt(1, charid);
+            ps.execute();
+            ps.close();
+            StringBuilder query = new StringBuilder();
+            query.append("INSERT INTO professions VALUES (");
+            query.append(charid);
+            query.append(", ");
+            query.append(professions.left.getType().ordinal());
+            query.append(", ");
+            query.append(professions.left.getLevel().ordinal());
+            query.append(", ");
+            query.append(professions.left.getExp());
+            query.append(")");
+            if (professions.right != null) {
+                query.append(",(");
+                query.append(charid);
+                query.append(", ");
+                query.append(professions.right.getType().ordinal());
+                query.append(", ");
+                query.append(professions.right.getLevel().ordinal());
+                query.append(", ");
+                query.append(professions.right.getExp());
+                query.append(")");
+            }
+            ps = con.prepareStatement(query.toString());
+            ps.execute();
+            ps.close();
+        } catch (SQLException e) {
+            System.out.println("Error."+e);
+        }
     }
 }
