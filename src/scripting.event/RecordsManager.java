b66e6d145cafec1918567dd340a89475fd97badc
SYJourney <ysfoig2153tg@hotmail.de>
2015-06-20 15:51:45 +0200

    Update RecordsManager.java
    added locks

--- src/scripting.event/RecordsManager.java
+++ src/scripting.event/RecordsManager.java
@@ -14,6 +14,7 @@ import java.util.Collections;
 import java.util.EnumMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.concurrent.locks.ReentrantLock;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import tools.DatabaseConnection;
@@ -27,6 +28,8 @@ public class RecordsManager {
     
     private final EnumMap<RecordEvent, List<Pair<String, Integer>>> records = new EnumMap<>(RecordEvent.class);
     private final byte world;
+    private final ReentrantLock recordsLoadLock;
+    private final ReentrantLock recordsUpdateLock;
     
     public enum RecordEvent {
         BOSSPQEASY, BOSSPQMED, BOSSPQHARD, BOSSPQHELL;
@@ -34,69 +37,91 @@ public class RecordsManager {
     
     public RecordsManager(byte world) {
         this.world = world;
+        recordsLoadLock = new ReentrantLock(false);
+        recordsUpdateLock = new ReentrantLock(true);
     }
     
     public List<Pair<String, Integer>> getRecords(RecordEvent event) {
-        return records.containsKey(event)?Collections.unmodifiableList(records.get(event)):null;
+        return records.containsKey(event)? Collections.unmodifiableList(records.get(event)) : null;
     }
     
-    public synchronized boolean updateRecords(RecordEvent event, String names, int time, int rank) {
-        Pair<String, Integer> toDelete = null;
-        
-        if (!records.containsKey(event)) {
-            List <Pair<String, Integer>> entries = new LinkedList<>();
-            entries.add(rank, new Pair<>(names, time));
-            records.put(event, entries);
-        } else {
-            List <Pair<String, Integer>> entries = records.get(event);
-            entries.add( new Pair<>(names, time));
-            if (records.get(event).size() > 15)
-                toDelete = entries.remove(15);
-            records.put(event, entries);
-        }
-        
+    public boolean updateRecords(RecordEvent event, String names, int time, int rank) {
+        recordsUpdateLock.lock();
         try {
-            Connection con = DatabaseConnection.getConnection();
-            PreparedStatement ps;
+            Pair<String, Integer> toDelete = null;
+            if (!records.containsKey(event)) {
+                List<Pair<String, Integer>> entries = new LinkedList<>();
+                entries.add(new Pair<>(names, time));
+                records.put(event, entries);
+            } else {
+                List<Pair<String, Integer>> entries = records.get(event);
+                
+                if (entries.get(rank).getRight() < time) //Check if rank was taken while locked
+                    entries.add(rank + 1, new Pair<>(names, time));
+                else
+                    entries.add(rank, new Pair<>(names, time));
+                if (records.get(event).size() > 15)
+                    toDelete = entries.remove(15);
+                records.put(event, entries);
+            }
+        
             try {
-                ps = con.prepareStatement("DELETE FROM records WHERE world = ?, event = ?, names = ?, time = ?");
+                Connection con = DatabaseConnection.getConnection();
+                PreparedStatement ps;
+                try {
+                    ps = con.prepareStatement("DELETE FROM records WHERE world = ? AND event = ? AND names = ? AND time = ?");
+                    ps.setInt(1, world);
+                    ps.setInt(2, event.ordinal());
+                    ps.setString(3, toDelete.getLeft());
+                    ps.setInt(4, toDelete.getRight());
+                    ps.execute();
+                    ps.close();
+                } catch (NullPointerException | SQLException ex) {
+                    Logger.getLogger(EventManager.class.getName()).log(Level.SEVERE, null, ex);
+                }
+                ps = con.prepareStatement("INSERT INTO records (`world`,`event`,`time`,`charnames`) VALUES (?, ?, ?, ?)");
                 ps.setInt(1, world);
                 ps.setInt(2, event.ordinal());
-                ps.setString(3, toDelete.getLeft());
-                ps.setInt(4, toDelete.getRight());
-                ps.execute();
+                ps.setInt(3, time);
+                ps.setString(4, names);
+                ps.executeUpdate();
                 ps.close();
-            } catch (NullPointerException | SQLException ex) {
+                return true;
+            } catch (SQLException ex) {
+                Logger.getLogger(EventManager.class.getName()).log(Level.SEVERE, null, ex);
+                return false;
             }
-            ps = con.prepareStatement("INSERT INTO records (`world`,`event`,`time`,`charnames`) VALUES (?, ?, ?, ?)");
-            ps.setInt(1, world);
-            ps.setInt(2, event.ordinal());
-            ps.setInt(3, time);
-            ps.setString(4, names);
-            ps.executeUpdate();
-            ps.close();
-            return true;
-        } catch (SQLException ex) {
-            Logger.getLogger(EventManager.class.getName()).log(Level.SEVERE, null, ex);
-            return false;
+        } finally {
+            recordsUpdateLock.unlock();
         }
     }
     
-    public boolean loadRecords(RecordEvent event) {
-        List <Pair<String, Integer>> entries = new LinkedList<>();
-        try (PreparedStatement ps = DatabaseConnection.getConnection().prepareStatement("SELECT time, charnames FROM records WHERE world = ? AND event = ? ORDER BY time ASC")) {
-            ps.setByte(1, world);
-            ps.setInt(2, event.ordinal());
-            try (ResultSet rs = ps.executeQuery()) {
-                while (rs.next()) {
-                    entries.add( new Pair<>(rs.getString("charnames"), rs.getInt("time")));
+    public List<Pair<String, Integer>> loadRecords(RecordEvent event) {
+        if (records.containsKey(event)) {
+            return records.get(event);
+        } else {
+            recordsLoadLock.lock();
+            try {
+                List<Pair<String, Integer>> entries = new LinkedList<>();
+                try (PreparedStatement ps = DatabaseConnection.getConnection().prepareStatement("SELECT time, charnames FROM records WHERE world = ? AND event = ? ORDER BY time ASC")) {
+                    ps.setByte(1, world);
+                    ps.setInt(2, event.ordinal());
+                    try (ResultSet rs = ps.executeQuery()) {
+                        while (rs.next()) {
+                            entries.add( new Pair<>(rs.getString("charnames"), rs.getInt("time")));
+                        }
+                        rs.close();
+                    }
+                    ps.close();
+                } catch (SQLException ex) {
+                    Logger.getLogger(EventManager.class.getName()).log(Level.SEVERE, null, ex);
+                    return null;
                 }
+                records.put(event, entries);
+                return entries;
+            } finally {
+                recordsLoadLock.unlock();
             }
-        } catch (SQLException ex) {
-            Logger.getLogger(EventManager.class.getName()).log(Level.SEVERE, null, ex);
-            return false;
         }
-        records.put(event, entries);
-        return !entries.isEmpty();
     }
 }
