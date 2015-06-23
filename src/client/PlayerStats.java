db608f9275080d82cf18dea655f4a99ebba267f1
SYJourney <ysfoig2153tg@hotmail.de>
2015-06-23 12:02:11 +0200

    Update PlayerStats.java

--- src/client/PlayerStats.java
+++ src/client/PlayerStats.java
@@ -1,7 +1,18 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright (C) 2015 SYJourney
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 package client;
@@ -22,9 +33,10 @@ import java.util.EnumMap;
 import tools.Randomizer;
 
 /**
- * JourneyMS
- * 
+ * Author: SYJourney
+ * This file is part of the Journey MMORPG Server
  */
+
 public class PlayerStats {
 
     private final EnumMap<MapleStat, Short> stats = new EnumMap<>(MapleStat.class);
@@ -85,6 +97,9 @@ public class PlayerStats {
         
         for (EnumMap.Entry<MapleStat, Short> entry : stats.entrySet()) {
             switch (entry.getKey()) {
+                case MP:
+                case HP:
+                    continue;
                 case STR:
                 case DEX:
                 case LUK:
@@ -172,8 +187,8 @@ public class PlayerStats {
                     break;
                 case JUMP:
                     if (newval > 123)
-                    newval = 123; 
-                        break;
+                        newval = 123; 
+                    break;
                 case MP:
                 case HP: 
                     if (newval > 30000)
