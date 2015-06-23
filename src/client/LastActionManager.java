ca31943eb9111960500f720675755b77bb4e3cfc
SYJourney <ysfoig2153tg@hotmail.de>
2015-06-23 12:01:49 +0200

    Update LastActionManager.java

--- src/client/LastActionManager.java
+++ src/client/LastActionManager.java
@@ -1,7 +1,18 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright (C) 2015 SYJourney
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 package client;
@@ -9,17 +20,22 @@ package client;
 import java.util.EnumMap;
 
 /**
- * JourneyMS
- * 
+ * Author: SYJourney
+ * This file is part of the Journey MMORPG Server
  */
+
 public class LastActionManager {
     
     private final EnumMap<MapleAction, Long> lastActionHolder;
     private short observedAction; 
     
     public enum MapleAction {
-        NPCTALK(500), HARVEST(1000);
+        NPCTALK(500), HARVEST, HEAL(1500), ITEMSORT, PETFOOD, CATCHITEM, SPECIALMOVE(500);
         private final int actionLimit;
+        
+        MapleAction() {
+            this(1000);
+        }
         
         MapleAction(int actionLimit) {
             this.actionLimit = actionLimit;
@@ -43,7 +59,7 @@ public class LastActionManager {
         if (lastActionHolder.containsKey(ma)) {
             if (System.currentTimeMillis() - lastActionHolder.get(ma) < 200) {
                 observedAction ++;
-                if (observedAction > 1000)
+                if (observedAction > 50)
                     return ActionResult.DISCONNECT;
             } else {
                 observedAction = (short) (observedAction/2);
